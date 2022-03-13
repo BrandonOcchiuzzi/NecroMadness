d313b9f8780fe61cbbc321379ba76b2041a2c6c4
TheMangoMaster <themangomaster.official@gmail.com>
2022-03-13 21:54:38 +0400

    Added Pause Menu & Music to Start Menu and Forest Scenes

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool gameIsPaused;
+
+    public GameObject pauseMenuUI;
+    public AudioMixer audioMixer;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gameIsPaused)
+            {
+                Resume();
+            } else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        gameIsPaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        gameIsPaused = true;
+    }
+
+    public void LoadOptions()
+    {
+        Debug.Log("Loading Options");
+    }
+    public void SetFullScreen(bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+
+    public void SetMusic(float music)
+    {
+        audioMixer.SetFloat("music", music);
+    }
+    public void SetSound(float sound)
+    {
+        audioMixer.SetFloat("sound", sound);
+    }
+
+    public void RestartGame()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("Village");
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("StartMenu");
+    }
+
+    public void ExitGame()
+    {
+        Debug.Log("Exit!");
+        Application.Quit();
+    }
+}
