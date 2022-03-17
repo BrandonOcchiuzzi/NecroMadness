a37e9f274831889be8dc41a7a8eb93cc29d12797
TheMangoMaster <themangomaster.official@gmail.com>
2022-03-17 21:16:01 +0400

    Completed New Village Scene

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -10,7 +10,7 @@ public class MainMenu : MonoBehaviour
 
     public void PlayGame()
     {
-        SceneManager.LoadScene("Village");
+        SceneManager.LoadScene("NewVillage");
     }
 
     public void QuitGame()
