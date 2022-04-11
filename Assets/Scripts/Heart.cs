8cf74cb04ae17673184f558b0eb2313062c5904f
BrandonOcchiuzzi <bocchiuz@my.centennialcollege.ca>
2022-04-11 12:26:16 -0400

    Movement, Hearts Broken, Camera weird
    player movement still weird on catacombs level at least
    hearts are now broken, collision isn't working?
    objects not rendering for some reason
    
    all needs to be fixed

--- Assets/Scripts/Heart.cs
+++ Assets/Scripts/Heart.cs
@@ -2,10 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Heart : Collidable
+public class Heart : MonoBehaviour
 {
     PlayerMover player;
     public int heal;
+
+    private void OnCollision2D(Collider2D coll)
+    {
+        if (coll.gameObject.name == "Player")
+        {
+            HeartHeal();
+        }
+    }
     public void HeartHeal()
     {
         //gain amount of health as specified on the prefab
